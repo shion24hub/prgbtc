30e29400253ba51d2e2abfd7823861c911f98fbf
Shion Noguchi <shion24stat@gmail.com>
2023-04-11 13:45:51 +0900

    finished ch3

--- work/ecc.py
+++ work/ecc.py
@@ -2,12 +2,25 @@
 楕円曲線暗号
 """
 
+from random import randint # 本番環境には絶対に使わない。
+from unittest import TestCase
+
+import hashlib
+import hmac
+
+
 P = 2 ** 256 - 2 ** 32 - 977 # secp256k1の法とする素数
 A = 0 # secp256k1のa
 B = 7 # secp256k1のb
 N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
 GX = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
 GY = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
+
+
+def hash256(s):
+    '''two rounds of sha256'''
+    return hashlib.sha256(hashlib.sha256(s).digest()).digest()
+
 
 class FieldElement:
     """
@@ -222,3 +235,72 @@ class S256Point(Point):
         coef = coefficient % N
 
         return super().__rmul__(coef)
+    
+    def verify(self, z, sig):
+        s_inv = pow(sig.s, N - 2, N)
+        u = z * s_inv % N
+        v = sig.r * s_inv % N
+        total = u * G + v * self
+
+        return total.x.num == sig.r
+
+
+G = S256Point(GX, GY)
+
+
+class Signature:
+
+    def __init__(self, r, s):
+        self.r = r
+        self.s = s
+
+    def __repr__(self):
+        return "Signature({:x}, {:x})".format(self.r, self.s)
+
+
+class PrivateKey:
+
+    def __init__(self, secret):
+
+        self.secret = secret
+        self.point = secret * G
+
+    def hex(self):
+
+        return "{:x}".format(self.secret).zfill(64)
+    
+    def sign(self, z):
+
+        # k = randint(0, N - 1) # 危険
+        k = self.deterministic_k(z)
+        r = (k * G).x.num
+        k_inv = pow(k, N - 2, N)
+        s = (z + r * self.secret) * k_inv % N
+
+        if s > N / 2:
+            s = N - s
+
+        return Signature(r, s)
+    
+    def deterministic_k(self, z):
+        # 暗号論的擬似乱数
+        k = b'\x00' * 32
+        v = b'\x01' * 32
+        if z > N:
+            z -= N
+        z_bytes = z.to_bytes(32, 'big')
+        secret_bytes = self.secret.to_bytes(32, 'big')
+        s256 = hashlib.sha256
+        k = hmac.new(k, v + b'\x00' + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        k = hmac.new(k, v + b'\x01' + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        while True:
+            v = hmac.new(k, v, s256).digest()
+            candidate = int.from_bytes(v, 'big')
+            if candidate >= 1 and candidate < N:
+                return candidate  # <2>
+            k = hmac.new(k, v + b'\x00', s256).digest()
+            v = hmac.new(k, v, s256).digest()
+
+    
